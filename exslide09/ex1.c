f0e41b6191613769c709b469f57cbde1cc3d6d27
emanuelle natal de moura <emanuellenmoura@gmail.com>
2025-11-18 14:07:40 -0300

    exercício 1 de listas

---
+++ exslide09/ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef struct Caixa {
+    int valor;
+    struct Caixa* prox;
+}caixa;
+
+
+int main(){
+	caixa c1, c2, c3, c4, c5;
+	c1.valor = 1; c2.valor = 3; c3.valor = 9; c4.valor = 7; c5.valor = 5;
+	c1.prox=&c2; c2.prox=&c3; c3.prox=&c4; c4.prox=&c5; c5.prox=NULL;
+	return 0;
+}
