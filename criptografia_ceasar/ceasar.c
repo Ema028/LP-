71a8881e4b5a4cf11a6bd9e6c26d0d4ee0d154ff
Emanuelle <emanuellenmoura@gmail.com>
2026-03-21 22:30:26 -0300

    pega input de tamanhos desconhecidos do usuário e troca as letras pela letra a x posições à frente na tabela ASCII, sendo x a chave numérica inserida pelo usuário

---
+++ criptografia_ceasar/ceasar.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int key_check(char* key);
+char rotate(char c, int n);
+
+int main(int argc, char* argv[])
+{
+    if(argc != 2)
+    {
+        printf("modo de uso: ./caesar chave\n");
+        return 1;
+    }
+    else
+    {
+        int verificador = key_check(argv[1]);
+        if(verificador == 1)
+        {
+            printf("modo de uso: ./caesar chave\n");
+            return 1;
+        }
+    }
+    
+    int key = atoi(argv[1]);
+    // forçar getline a usar malloc
+    char* text = NULL;
+    // contar tamanho do buffer alocado sem exceder UINT_MAX
+    size_t len = 0;
+    // signed integer para contar char lido porcausa de casos de EOF ou erro 
+    ssize_t read;
+    printf("Texto para criptografar:\n");
+    read = getline(&text, &len, stdin);
+ 
+    for(int i = 0; i < read; i++)
+    {
+        text[i] = rotate(text[i], key);
+    }
+    printf("Texto criptografado: %s", text);
+    free(text);
+    return 0;
+}
+
+int key_check(char* key)
+{
+    int len = strlen(key);
+    for(int i = 0; i < len; i++)
+    {
+        if(!isdigit(key[i]))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
+
+char rotate(char c, int n)
+{
+    if(islower(c))
+    {
+        // valor ascii para a = 97, A = 65, 26 letras no alfabeto 
+        c = (((c - 'a') + n) % 26) + 'a';
+    }
+    else if(isupper(c))
+    {
+        c = (((c - 'A') + n) % 26) + 'A';
+    }
+    return c;
+}
+
